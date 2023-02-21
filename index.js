89ddfc47ba6c8d521d193cbc2bbfd8e3e1eb8ed4
Dev-Sarmad <sarmadh230@gmail.com>
2023-02-21 14:56:27 +0500

    flexvoxes

---
+++ index.js
@@ -0,0 +1,20 @@
+const tabs = document.querySelectorAll(".tab");
+const pannels = document.querySelectorAll(".pannel");
+
+tabs.forEach((tab) => tab.addEventListener("click", ontab));
+function ontab(e) {
+  tabs.forEach((tab) => {
+    // deactivate the tab
+    tab.children[0].classList.remove("border-softRed", "border-b-4");
+  });
+
+  // deactivate  the pannels
+  pannels.forEach((pannel) => pannel.classList.add("hidden"));
+  //   activate the pannel and the tab based on the target attribute
+  e.target.classList.add("border-softRed", "border-b-4");
+  let pannel = e.target.getAttribute("data-target");
+  document
+    .getElementById("pannel")
+    .getElementsByClassName(pannel)[0]
+    .classList.remove("hidden");
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    screens:{
-      sm:"480px",
-      md:"768px",
-      lg:"1020px",
-      xl:"1440px",
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1020px",
+      xl: "1440px",
     },
     extend: {
-      colors:{
-        softBlue:"hsl(231,69%,60%)",
-        softRed:"hsl(0,94%,66%)",
-        grayisBlue:"hsl(229,8%,60%)",
-        verydarktBlue:"hsl(229,31%,21%)",
+      colors: {
+        softBlue: "hsl(231,69%,60%)",
+        softRed: "hsl(0,94%,66%)",
+        grayisBlue: "hsl(229,8%,60%)",
+        verydarktBlue: "hsl(229,31%,21%)",
       },
-      fontFamily:{
-        sans:["Rubik", "sans-serif"]
-      }
+      fontFamily: {
+        sans: ["Rubik", "sans-serif"],
+      },
+      
     },
   },
   plugins: [],
-}
+};
